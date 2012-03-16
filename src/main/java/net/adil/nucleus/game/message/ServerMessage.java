39463ee6e9ec6d78e4591ea595fd1f75c497c520
Adil Hafeez <adil.photon@gmail.com>
2012-03-16 18:10:13 +0000

    Updated ServerMessage with a small plan

--- src/main/java/net/adil/nucleus/game/message/ServerMessage.java
+++ src/main/java/net/adil/nucleus/game/message/ServerMessage.java
@@ -1,5 +1,17 @@
 package net.adil.nucleus.game.message;
 
-public class ServerMessage {
+public class ServerMessage 
+{
+//handle server->client message stuff here:
+/*
+Append:
+short
+byte
+object
+char[]
+string
+int
+long
 
+*/
 }
