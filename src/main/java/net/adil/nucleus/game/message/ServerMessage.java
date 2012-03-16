a7541f726bd192fde2c3e354178542fda7cc5413
Jinz0 <AHafeez.97@gmail.com>
2012-03-16 18:18:26 +0000

    small servermessage update

--- src/main/java/net/adil/nucleus/game/message/ServerMessage.java
+++ src/main/java/net/adil/nucleus/game/message/ServerMessage.java
@@ -1,5 +1,29 @@
 package net.adil.nucleus.game.message;
 
-public class ServerMessage {
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBuffers;
 
+
+public class ServerMessage 
+{
+    private ChannelBuffer body;
+	private int id;
+	
+	private int MESSAGE_SIZE=1024;
+	
+	
+	public ServerMessage(int id)
+	{
+		this.id = id;
+		this.body = ChannelBuffers.dynamicBuffer(this.MESSAGE_SIZE);
+	}
+	
+	public void AppendString(String message)
+	{
+		this.body.writeBytes(message.getBytes());
+	}
+	public void AppendInt32(int num)
+	{
+		this.body.writeInt(num >> 32); //shifts 32 places?
+	}
 }
